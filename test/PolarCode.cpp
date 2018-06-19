a82413c171afb34e7e89ad812452b9cf7b764276
egs33 <egs33.bicycle@gmail.com>
2018-06-19 11:12:13 +0900

    Add test for decode

--- test/PolarCode.cpp
+++ test/PolarCode.cpp
@@ -29,3 +29,35 @@ TEST(encode, length_64) {
 }
 
 
+TEST(decode, bec_length_4) {
+    BEC bec(0.5);
+    PolarCode code(4, 2, bec);
+    const std::vector<int> expected1 = {0, 0};
+    ASSERT_EQ(code.SuccessiveCancellationDecode({0, 0, 0, 0}), expected1);
+    ASSERT_EQ(code.LlrSCDecode({0, 0, 0, 0}), expected1);
+
+    const std::vector<int> expected2 = {0, 0};
+    ASSERT_EQ(code.SuccessiveCancellationDecode({0, 0, 0, 2}), expected2);
+    ASSERT_EQ(code.LlrSCDecode({0, 0, 0, 2}), expected2);
+
+    const std::vector<int> expected3 = {0, 1};
+    ASSERT_EQ(code.SuccessiveCancellationDecode({2, 1, 1, 1}), expected3);
+    ASSERT_EQ(code.LlrSCDecode({2, 1, 1, 1}), expected3);
+}
+
+
+TEST(decode, bec_length_64) {
+    BEC bec(0.5);
+    PolarCode code(64, 20, bec);
+    const std::vector<int> expected1(20, 0);
+    ASSERT_EQ(code.SuccessiveCancellationDecode({0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}),expected1);
+    ASSERT_EQ(code.LlrSCDecode({0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}), expected1);
+
+    const std::vector<int> expected2(20, 0);
+    ASSERT_EQ(code.SuccessiveCancellationDecode({2,2,2,2,0,0,2,0,0,0,2,0,2,2,2,2,0,0,0,0,0,0,2,0,0,0,2,0,2,2,0,2,0,2,0,0,0,2,0,2,0,2,0,2,2,0,0,2,2,2,2,0,2,2,0,2,0,0,2,0,0,0,0,2}), expected2);
+    ASSERT_EQ(code.LlrSCDecode({2,2,2,2,0,0,2,0,0,0,2,0,2,2,2,2,0,0,0,0,0,0,2,0,0,0,2,0,2,2,0,2,0,2,0,0,0,2,0,2,0,2,0,2,2,0,0,2,2,2,2,0,2,2,0,2,0,0,2,0,0,0,0,2}), expected2);
+
+    const std::vector<int> expected3 = { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0 };
+    ASSERT_EQ(code.SuccessiveCancellationDecode({0,2,2,0,1,2,0,2,0,2,2,0,1,2,2,2,0,2,2,2,2,0,0,1,2,0,2,2,2,1,2,0,2,0,1,2,1,0,1,2,2,0,2,2,1,0,2,0,1,0,1,0,1,2,1,2,2,2,1,0,2,2,2,2,}), expected3);
+    ASSERT_EQ(code.LlrSCDecode({0,2,2,0,1,2,0,2,0,2,2,0,1,2,2,2,0,2,2,2,2,0,0,1,2,0,2,2,2,1,2,0,2,0,1,2,1,0,1,2,2,0,2,2,1,0,2,0,1,0,1,0,1,2,1,2,2,2,1,0,2,2,2,2,}), expected3);
+}
